95a90b1276236ab97601710377e4e7b2d8146dc8
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-02 15:37:32 -0600

    Create metronome.cpp

---
+++ metronome.cpp
@@ -0,0 +1,32 @@
+//metronome
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = b; i >= a; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+
+using namespace std;
+
+
+int main(){
+
+
+
+    double n;
+    cin >> n;
+
+    printf("%.2f\n",n/4);
+    return 0;
+}
