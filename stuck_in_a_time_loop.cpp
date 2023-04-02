479f9cfccf3efa3242dcc5815367d3ae7298c03c
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-02 16:33:40 -0600

    Create stuck_in_a_time_loop.cpp

---
+++ stuck_in_a_time_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int i;
+    
+    cin >> i;
+    
+    for(int x = 0; x < i; x++){
+        cout << x + 1 << " Abracadabra\n";
+    }
+    
+    return 0;
+}
