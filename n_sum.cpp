b89e86ab3be7e07db2e018704ceba849dbc1edd5
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-02 16:05:07 -0600

    Create n_sum.cpp

---
+++ n_sum.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<cmath>
+#include<climits>
+#include<queue>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = b; i >= a; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+
+using namespace std;
+
+typedef vector<int> vi;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int N;
+    cin >> N;
+
+    int out = 0;
+    FOR(i,0,N){
+        int temp;
+        cin >> temp;
+
+        out += temp;
+    }
+
+    cout << out << endl;
+
+    return 0;
+}
