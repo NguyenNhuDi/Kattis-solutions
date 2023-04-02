b90a66e8050d78b1225db7d2230ea79318229a87
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-02 15:42:47 -0600

    Create boss_battle.cpp

---
+++ boss_battle.cpp
@@ -0,0 +1,46 @@
+//Boss Battle
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = b; i >= a; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int out = 0;
+
+    while(1){
+        out++;
+        n -= 3;
+        if(n <= 0){
+            break;
+        }
+        n += 2;
+    }
+    cout << out << endl;
+
+
+    return 0;
+}
