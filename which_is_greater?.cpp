65000e7fae6d148b1ddd68ab36562ce41948588f
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-02 16:04:46 -0600

    Create which_is_greater?.cpp

---
+++ which_is_greater?.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<cmath>
+#include<climits>
+#include<queue>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = b; i >= a; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+
+using namespace std;
+
+typedef vector<int> vi;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
