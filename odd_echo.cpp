0676c7260c53c1395df809056190d0e661390b38
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-29 07:19:43 +0700

    Create odd_echo.cpp

---
+++ odd_echo.cpp
@@ -0,0 +1,40 @@
+//odd echo
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = a; i >= b; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+#define MOD 0x3b9aca07
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    FOR(i,0,n){
+        string in;
+        cin >> in;
+        if(i % 2 == 0){
+            cout << in << endl;
+        }
+    }
+
+    return 0;
+}
