9cc320eca74badad76569797a0bfa7923dc62737
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-29 08:11:05 +0700

    Create digit_swap.cpp

---
+++ digit_swap.cpp
@@ -0,0 +1,38 @@
+//digit swap
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = a; i >= b; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+#define MOD 0x3b9aca07
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string input;
+    cin >> input;
+
+    FORD(i,1,0){
+        cout << input[i];
+    }
+    cout << endl;
+
+    return 0;
+}
