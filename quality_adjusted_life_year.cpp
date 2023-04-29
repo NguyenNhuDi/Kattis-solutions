15d7c70d81f41942cce745f43ce89080ca63a555
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-04-29 07:55:24 +0700

    Create quality_adjusted_life_year.cpp

---
+++ quality_adjusted_life_year.cpp
@@ -0,0 +1,41 @@
+//quality adjusted life year
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = a; i >= b; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+#define MOD 0x3b9aca07
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    double out = 0;
+    FOR(i,0,n){
+        double q, years; cin >> q >> years;
+        out += q * years;
+
+    }
+    printf("%.4f\n",out);
+    
+
+    return 0;
+}
