f2fef071a2664ccc6358f97b5251d79ef641a5f6
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-09-05 23:52:25 -0600

    Create adding_trouble.cpp

---
+++ adding_trouble.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<unordered_map>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = a; i >= b; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define foreach(x,y) for(const auto & x : y)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+#define MOD 0x3b9aca07
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, c; cin >> a >> b >> c;
+
+    if (a + b == c){
+        cout << "correct!" << endl;
+    } 
+    else{
+        cout << "wrong!" << endl;
+    }
+
+
+    return 0;
+}
