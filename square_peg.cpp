ab3ceafddd66b245c4e2e3b63bdbc25eafb8a228
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-09-08 13:37:31 -0600

    Create square_peg.cpp

---
+++ square_peg.cpp
@@ -0,0 +1,41 @@
+// square peg
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<unordered_map>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<map>
+
+#define FOR(i,a,b) for(int i = a; i < b; i++)
+#define FORD(i,a,b) for(int i = a; i >= b; i--)
+#define FORE(i,a,b) for(int i = a; i <=b; i++)
+#define fe(x,y) for(const auto & x : y)
+#define SIZE(x) x.size()
+#define vi vector<int>
+#define usi unordered_set<int>
+#define si set<int>
+#define MOD 0x3b9aca07
+
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double l, r; cin >> l >> r;
+
+    double c = sqrt((l * l) * 2);
+
+    string out = c > r * 2 ? "nope\n" : "fits\n";
+
+    cout << out; 
+
+    return 0;
+}
