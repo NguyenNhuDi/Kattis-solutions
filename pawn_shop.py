fdb3a9857bec008e11e3c640c3ab043c58ea9a97
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2024-01-12 17:53:40 -0700

    Update pawn_shop.py

--- pawn_shop.py
+++ pawn_shop.py
@@ -18,15 +18,8 @@ def main():
         oV = origin[r]
         nV = new[r]
 
-        if oV in oSet:
-            oSet[oV] += 1
-        else:
-            oSet[oV] = 1
-
-        if nV in nSet:
-            nSet[nV] += 1
-        else:
-            nSet[nV] = 1
+        oSet[oV] = 1 if oV not in oSet else oSet[oV] + 1
+        nSet[nV] = 1 if nV not in nSet else nSet[nV] + 1
 
         if oSet == nSet and r < n - 1:
             print('#', end=' ')
