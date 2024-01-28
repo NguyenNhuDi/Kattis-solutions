d20852248f2216ed2e2cec318327f0b0b4fb446a
NguyenNhuDi <dinguyennhu@gmail.com>
2024-01-28 14:29:26 -0700

    format test

--- updater.py
+++ updater.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     cppLen = len(cppFile)
 
 
-    f.write(f'Total problems solved: {pLen + cppLen}\n')
-    f.write(f'Total problems solved with Python: {pLen}\n')
-    f.write(f'Total problems solved with C++: {cppLen}\n')
+    f.write(f'Total problems solved: {pLen + cppLen}  \n')
+    f.write(f'Total problems solved with Python: {pLen}  \n')
+    f.write(f'Total problems solved with C++: {cppLen}  \n')
 
     f.close()
